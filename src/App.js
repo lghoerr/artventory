949a36141c765aa6cc5331927779b2ee1e33342c
Lucia Hoerr <63504311+lghoerr@users.noreply.github.com>
2023-12-18 10:17:39 -0700

    Preliminary logo and title/name changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>ARTVENTORY</h1>
+          Free inventory tracking for independent artists.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Artventory
+        </a> */}
       </header>
     </div>
   );
